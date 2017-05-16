7b4f17578b49ca6099773cf6617c6ccb56a1bec6
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2017-05-16 13:49:17 -0300

    Extract css to a dedicated file

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 let config = {
   entry: './src/index.js',
@@ -21,10 +22,16 @@ let config = {
       },
       {
         test: /\.scss$/, //files wnding with .scss
-        loader: ['style-loader', 'css-loader', 'sass-loader']
+        use: ExtractTextWebpackPlugin.extract({ // call our plugin with extract method
+          use: ['css-loader', 'sass-loader'], // use these loaders
+          fallback: 'style-loader' // fallback for any CSS not extracted
+        }) // end extract
       }
-    ]
-  }
+    ] // end rules
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin('styles.css') // call the ExtractTextWebpackPlugin and name ou css file
+  ]
 }
 
 module.exports = config;
